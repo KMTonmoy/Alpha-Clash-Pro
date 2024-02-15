62aa7f74062655a59e8af72d00642abd39566423
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-02-15 18:39:23 +0600

    Keyboard Event handelar sigature

--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,49 @@
 
 function handelKeybordKeyUpEvent(event) {
     const playerPressed = event.key;
-
+    console.log("Player Pressed", playerPressed)
+    if (playerPressed === "Escape") {
+        gameOver()
+    }
     const currentAlphabetElement = document.getElementById('current-Alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
     if (playerPressed === expectedAlphabet) {
 
-        const currentScoreElement = document.getElementById("current-Score")
-        const currentScoreText = currentScoreElement.innerText
-        const currentScore = parseInt(currentScoreText)
-        const newScore = currentScore + 1;
-        currentScoreElement.innerText = newScore;
+        const currentScore = getTextElementValeById("current-Score")
+        const updateScore = currentScore + 1;
+        setTextElementVlueById("current-Score", updateScore);
+
+
+
+        //^ ===================================================================
+        // const currentScoreElement = document.getElementById("current-Score")
+        // const currentScoreText = currentScoreElement.innerText
+        // const currentScore = parseInt(currentScoreText)
+        // const newScore = currentScore + 1;
+        // currentScoreElement.innerText = newScore;
+
         removeBackgroundColorById(expectedAlphabet)
         continueGame()
     } else {
-        console.log("Dhur Right Key Press Koro")
+        const currentLife = getTextElementValeById("current-Life")
+        const updateLife = currentLife - 1
+        setTextElementVlueById("current-Life", updateLife)
 
-        const currentLifeElement = document.getElementById("current-Life")
-        const currentLifeText = currentLifeElement.innerText
-        const currentLife = parseInt(currentLifeText)
-        const newLife = currentLife - 1;
-        currentLifeElement.innerText = newLife;
+
+        if (updateLife === 0) {
+            gameOver();
+        }
+
+
+        //^ ==================================================
+        // console.log("Dhur Right Key Press Koro")
+
+        // const currentLifeElement = document.getElementById("current-Life")
+        // const currentLifeText = currentLifeElement.innerText
+        // const currentLife = parseInt(currentLifeText)
+        // const newLife = currentLife - 1;
+        // currentLifeElement.innerText = newLife;
     }
 }
 document.addEventListener("keyup", handelKeybordKeyUpEvent);
@@ -39,8 +61,23 @@ function continueGame() {
 }
 
 function play() {
+
+
     hideElementById("home-Screen");
+    hideElementById("ScoreGround");
     showElementById("playGorund");
+    setTextElementVlueById("current-Life", 5)
+    setTextElementVlueById("current-Score", 0)
     continueGame();
 
+}
+function gameOver() {
+    hideElementById('playGorund')
+    showElementById('ScoreGround')
+    const lastScore = getTextElementValeById("current-Score")
+    console.log(lastScore)
+    setTextElementVlueById("game-Score", lastScore)
+    const CurrentAlphabet = getElementTextById("current-Alphabet")
+    console.log(CurrentAlphabet)
+    removeBackgroundColorById(CurrentAlphabet)
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -12,28 +12,39 @@ function setBackgroundColorById(elementId) {
     element.classList.add("bg-orange-400")
 
 }
- 
+
 function removeBackgroundColorById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.remove("bg-orange-400")
 
 }
- 
- 
 
 
+
+
+function getTextElementValeById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+function setTextElementVlueById(elementId, value) {
+    const element = document.getElementById(elementId)
+    element.innerText = value;
+}
+
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId)
+    const text = element.innerText
+    return text
+}
 
 
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
     const alphabets = alphabetString.split('')
-    // console.log(alphabets)
-
     const randomNumber = Math.random() * 25
     const index = Math.round(randomNumber)
-    // console.log(index)
-
     const alphabet = alphabets[index];
-    // console.log(index, alphabet);
     return alphabet;
 }
